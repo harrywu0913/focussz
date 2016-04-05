b3ff68059c9644c9f246e900491fb3e9fea73090
wyfxuantong <350851513@qq.com>
2016-04-05 23:05:46 +0800

    d

--- mkfs/mkfs-login/src/main/java/com/focuszz/login/authenticator/AccessAuthenticator.java
+++ mkfs/mkfs-login/src/main/java/com/focuszz/login/authenticator/AccessAuthenticator.java
@@ -2,8 +2,22 @@ package com.focuszz.login.authenticator;
 
 public class AccessAuthenticator implements Authenticator {
 
+    private String configLocation;
+
+    public AccessAuthenticator(String configLocation) {
+        this.configLocation = configLocation;
+    }
+
     public boolean permit(String role, String url) {
         return false;
+    }
+
+    public String getConfigLocation() {
+        return configLocation;
+    }
+
+    public void setConfigLocation(String configLocation) {
+        this.configLocation = configLocation;
     }
 
 }
--- mkfs/mkfs-login/src/main/java/com/focuszz/login/authenticator/factory/XmlAuthenticationDefinitionReader.java
+++
@@ -1,5 +0,0 @@
-package com.focuszz.login.authenticator.factory;
-
-public class XmlAuthenticationDefinitionReader implements AuthenticationDefinitionReader {
-
-}
